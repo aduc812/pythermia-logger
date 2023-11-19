5efb2d14e74d00b04a2b43da3f94e3606adbce22
Sergey <omelkovs@gmail.com>
2023-11-19 22:52:59 +0200

    research into sql

---
+++ src/pythermia-logger/logger.py
@@ -0,0 +1,118 @@
+# from nordpool import elspot
+
+# prices_spot = elspot.Prices()
+
+# pprint(prices_spot.hourly(areas=["EE"]))
+
+import asyncio
+import logging
+from sys import argv
+import sqlite3
+
+from pythermiagenesis import ThermiaGenesis
+from pythermiagenesis import ThermiaConnectionError
+from pythermiagenesis.const import (
+    REGISTERS,
+    REG_INPUT,
+    KEY_ADDRESS,
+    KEY_DATATYPE,
+    ATTR_COIL_ENABLE_HEAT,
+    ATTR_COIL_ENABLE_BRINE_IN_MONITORING,
+    TYPE_BIT,
+    TYPE_INT,
+    TYPE_UINT,
+    TYPE_LONG,
+    TYPE_STATUS,
+)
+
+
+def dtp_convert(val):
+    if type(val) is string:
+        return "TEXT NOT NULL"
+    if type(val) is int:
+        return "INTEGER NOT NULL"
+    if type(val) is float:
+        return "FLOAT NOT NULL"
+
+
+# heatpum IP address/hostname
+HOST = "10.0.20.8"
+PORT = 502
+# logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+
+create_table_header = """ CREATE TABLE parameters(
+ID INTEGER PRIMARY KEY AUTOINCREMENT,
+"""
+
+
+def create_table(data_items):
+    dblines = []
+    for i, (name, val) in enumerate(data_items):
+        dtp_txt = dtp_convert(val)
+        dblines.append(f"{name}  {dtp_txt}")
+    dbreq = create_table_header + ",\n".join(dblines) + "\n)"
+    print(dbreq)
+
+
+async def main():
+    host = argv[1] if len(argv) > 1 else HOST
+    port = argv[2] if len(argv) > 2 else PORT
+    kind = argv[3] if len(argv) > 3 else "inverter"
+    prot = argv[4] if len(argv) > 4 else "TCP"
+
+    # RTU arguments; leave default for TCP connection
+    baud = int(argv[5]) if len(argv) > 5 else 19200
+    btsz = int(argv[6]) if len(argv) > 6 else 8
+    prty = argv[7] if len(argv) > 7 else "E"
+    stbt = int(argv[8]) if len(argv) > 8 else 1
+    echo = bool(argv[9]) if len(argv) > 9 else False
+
+    # argument kind: inverter - for Diplomat Inverter
+    #                mega     - for Mega
+    # argument prot: "TCP"    - for TCP/IP
+    #                "RTU"    - for RTU over RS485
+
+    thermia = ThermiaGenesis(
+        host,
+        protocol=prot,
+        port=port,
+        kind=kind,
+        delay=0.15,
+        baudrate=baud,
+        bytesize=btsz,
+        parity=prty,
+        stopbits=stbt,
+        handle_local_echo=echo,
+    )
+    try:
+        # Get all register types
+        # await thermia.async_update()
+        # Get only input registers
+        # await thermia.async_update([REG_INPUT])
+        # Get one specific register
+        # await thermia.async_update(only_registers=[ATTR_COIL_ENABLE_BRINE_IN_MONITORING, ATTR_COIL_ENABLE_HEAT])
+        await thermia.async_update([])
+
+    except ThermiaConnectionError as error:
+        print(f"Failed to connect: {error.message}")
+        return
+    except ConnectionError as error:
+        print(f"Connection error {error}")
+        return
+
+    if thermia.available:
+        print(f"Data available: {thermia.available}")
+        print(f"Model: {thermia.model}")
+        print(f"Firmware: {thermia.firmware}")
+        # for i, (name, val) in enumerate(thermia.data.items()):
+        #    print(f"{REGISTERS[name][KEY_ADDRESS]}\t{name}\t{val}")
+        create_table_req = create_table(thermia.data.items())
+        con = sqlite3.connect("thermia-log.db")
+        cur = con.cursor()
+        # cur.execute(create_table_req)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
